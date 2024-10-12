65b24d74968c16725b02c7e4190bd27165948edf
BORRAACH <eduardorodriguesp254@gmail.com>
2024-10-12 11:25:58 -0300

    firs idea of the vertical header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div className="p-2 fixed left-1 top-1 bottom-1 min-h-[100%] bg-slate-200 rounded-[0.150rem] flex flex-col justify-center gap-5">
+      <div className="p-5 bg-red-500 rounded-sm"></div>
+      <div className="p-5 bg-red-500 rounded-sm"></div>
+      <div className="p-5 bg-red-500 rounded-sm"></div>
+      <div className="p-5 bg-red-500 rounded-sm"></div>
+    </div>
+  );
+}
