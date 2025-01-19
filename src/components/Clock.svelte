fe11665fd194a50303422bb8ee5bba67b410ae85
BORRAACH <eduardorodriguesp254@gmail.com>
2025-01-19 13:14:06 -0300

    adicionando um mostrador de horas restantes ate a prova desse ano; adapta um design responsivo para o site;

--- src/components/Clock.svelte
+++ src/components/Clock.svelte
@@ -1,64 +1,145 @@
 <script lang="ts">
+  let thisYear = $state(false)
+
   class Time {
     static currentDate = new Date();
-    static finalDate = new Date("2029-10-13");
-    static differenceOfMiliseconds =
-      this.finalDate.getTime() - this.currentDate.getTime();
+    static timeLeftThisYear = new Date("2025-10-13")
+    static fullTimeLeft = new Date("2029-10-13");
+
+    static differenceOfMilisecondsToFinalTest =
+      this.fullTimeLeft.getTime() - this.currentDate.getTime();
+    static differenceOfMilisecondsThisYearTest = 
+      this.timeLeftThisYear.getTime() - this.currentDate.getTime();
 
     static getYearsLeft() {
       const differenceInYears =
-        this.finalDate.getFullYear() - this.currentDate.getFullYear();
+        this.fullTimeLeft.getFullYear() - this.currentDate.getFullYear();
       return differenceInYears;
     }
 
     static getMonthsLeft() {
       const differenceInMonths =
-        this.differenceOfMiliseconds / (30 * 24 * 60 * 60 * 1000);
+        this.differenceOfMilisecondsToFinalTest / (30 * 24 * 60 * 60 * 1000);
       return Math.floor(differenceInMonths);
     }
 
     static getDaysLeft() {
       const differenceOfDays =
-        this.differenceOfMiliseconds / (24 * 60 * 60 * 1000);
+        this.differenceOfMilisecondsToFinalTest / (24 * 60 * 60 * 1000);
       return Math.floor(differenceOfDays);
     }
 
     static getHoursLeft() {
-      const differenceOfHours = this.differenceOfMiliseconds / (60 * 60 * 1000);
+      const differenceOfHours = this.differenceOfMilisecondsToFinalTest / (60 * 60 * 1000);
       return Math.floor(differenceOfHours);
     }
-  }
-</script>
+    
+    static getRemainingTimeThisYearInYears() {
+      const differenceInYears = 
+        this.timeLeftThisYear.getFullYear() - this.currentDate.getFullYear();
+      return differenceInYears;
+    }
 
-<div class="mt-40">
-  <div class="flex gap-5 justify-center dark:text-neutral-400">
-    <div class="backdrop-blur-sm rounded-md p-10 flex justify-center">
-      <h1 class="text-3xl font-extrabold">
-        {Time.getYearsLeft()} anos
-      </h1>
+    static getRemainingTimeThisYearInMonths() {
+      const differenceInMonths = 
+        this.differenceOfMilisecondsThisYearTest / (30 * 24 * 60 * 60 * 1000);
+      return Math.floor(differenceInMonths);
+    }
+
+    static getRemainingTimeThisYearInDays() {
+      const differenceInDays = 
+        this.differenceOfMilisecondsThisYearTest / (24 * 60 * 60 * 1000);
+      return Math.floor(differenceInDays)
+    }
+
+    static getRemainingTimeThisYearInHours() {
+      const differenceInHours = 
+        this.differenceOfMilisecondsThisYearTest / (60 * 60 * 1000)
+      return Math.floor(differenceInHours)
+    }
+  }  
+  
+  function setThisYear() { 
+    if (thisYear) thisYear = false;
+    else thisYear = true    
+  }
+  
+</script>
+<div class="transition-all">
+  {#if !thisYear}
+    <div class="mt-40">
+      <div class="flex flex-col gap-5 lg:justify-center md:flex-row dark:text-neutral-400">
+        {#if Time.getYearsLeft() !== 0}
+          <div class="rounded-md p-10 flex justify-center">
+            <h1 class="text-3xl font-extrabold">
+              {Time.getYearsLeft()} anos
+            </h1>
+          </div>
+        {/if}
+        <div class="rounded-md p-10 flex justify-center">
+          <h1 class="text-3xl font-extrabold">
+            {Time.getMonthsLeft()} meses
+          </h1>
+        </div>
+        <div class="rounded-md p-10 flex justify-center">
+          <h1 class="text-3xl font-extrabold">
+            {Time.getDaysLeft()} dias
+          </h1>
+        </div>
+        <div class="rounded-md p-10 flex justify-center">
+          <h1 class="text-3xl font-extrabold">
+            {Time.getHoursLeft()} horas
+          </h1>
+        </div>
+      </div>
+      <div class="flex justify-center">
+        <h4 class="text-slate-500 dark:text-neutral-600 mt-5">
+          Este é o tempo em anos, meses, dias e horas até minha ultima tentativa na
+          prova do ita
+        </h4>
+      </div>
     </div>
-    <div class="backdrop-blur-sm rounded-md p-10 flex justify-center">
-      <h1 class="text-3xl font-extrabold">
-        {Time.getMonthsLeft()} meses
-      </h1>
+  {/if}
+
+  {#if thisYear}
+    <div class="mt-40">
+      <div class="flex flex-col gap-5 justify-center md:flex-row dark:text-neutral-400">
+        {#if Time.getRemainingTimeThisYearInYears() !== 0}
+          <div class="rounded-md p-10 flex justify-center">
+            <h1 class="text-3xl font-extrabold">
+              {Time.getRemainingTimeThisYearInYears()} anos
+            </h1>
+          </div>
+        {/if}
+        <div class="rounded-md p-10 flex justify-center">
+          <h1 class="text-3xl font-extrabold">
+            {Time.getRemainingTimeThisYearInMonths()} meses
+          </h1>
+        </div>
+        <div class="rounded-md p-10 flex justify-center">
+          <h1 class="text-3xl font-extrabold">
+            {Time.getRemainingTimeThisYearInDays()} dias
+          </h1>
+        </div>
+        <div class="rounded-md p-10 flex justify-center">
+          <h1 class="text-3xl font-extrabold">
+            {Time.getRemainingTimeThisYearInHours()} horas
+          </h1>
+        </div>
+      </div>
+      <div class="flex justify-center">
+        <h4 class="text-slate-500 dark:text-neutral-600 mt-5">
+          Este é o tempo em anos, meses, dias e horas até minha prova do ita desse ano
+        </h4>
+      </div>
     </div>
-    <div class="backdrop-blur-sm rounded-md p-10 flex justify-center">
-      <h1 class="text-3xl font-extrabold">
-        {Time.getDaysLeft()} dias
-      </h1>
-    </div>
-    <div class="backdrop-blur-sm rounded-md p-10 flex justify-center">
-      <h1 class="text-3xl font-extrabold">
-        {Time.getHoursLeft()} horas
-      </h1>
-    </div>
-  </div>
-  <div class="flex justify-center">
-    <h4 class="text-slate-500 dark:text-neutral-600 mt-5">
-      Este é o tempo em anos, meses, dias e horas até minha ultima tentativa na
-      prova do ita
-    </h4>
-  </div>
+    {/if}
+
+   <div class="flex justify-center items-center py-10">
+    <button onclick={setThisYear} class="text-neutral-900 px-2 py-1 rounded-md bg-neutral-400">
+      {thisYear ? "Ultimo ano" : "Esse ano"}
+    </button>
+   </div>
 </div>
 
 <style>
@@ -67,7 +148,4 @@
       rgba(255, 255, 255, 1) -3px -3px 6px 1px inset;
   }
 
-  .intern-shadow {
-    box-shadow: var(--shadow);
-  }
 </style>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,8 @@
-<header class="backdrop-blur-sm flex justify-between items-center p-6">
-  <ul class="flex gap-10 dark:text-slate-100">
+<header class="sticky top-0 backdrop-blur-sm flex justify-between items-center p-6">
+  <div class="block md:hidden">
+      <i class="fa-solid fa-bars dark:text-neutral-400 text-2xl active:text-neutral-300"></i>
+  </div>
+  <ul class="hidden gap-10 dark:text-slate-100 md:flex">
     <li>
       <a href="/"> Home </a>
     </li>
@@ -7,9 +10,8 @@
       <a href="/about"> About </a>
     </li>
     <li>
-      <a href="#"> Example </a>
+      <a href="/"> Example </a>
     </li>
   </ul>
 </header>
 
-<slot />
